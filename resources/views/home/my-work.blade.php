1034b0ec7ec42d2db6f0a63a7d6e0b065bbfaeab
Jesse McIntire <jessemcintire2@gmail.com>
2017-10-30 12:58:06 -0400

    tweak my work section content layout; make header, hero, my expertise, quote, my work, contact cta, picture window sections

--- resources/views/home/my-work.blade.php
+++ resources/views/home/my-work.blade.php
@@ -10,164 +10,43 @@
     </div>
     <ul class="my-work__list">
       <li class="my-work__item">
+        <div class="item-content">
+          <div class="item-description">
+            <h3>
+              Example Company
+            </h3>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit Lorem ipsum dolor sit amet, consectetur adipiscing elit
+            </p>
+          </div>
+          <div class="item-info">
+            <a href="#" class="button-link button-link--primary">
+              Visit the website
+              <img src="{{ asset('images/svg/thin-arrow-right-teal.svg') }}" alt="">
+            </a>
+            <ul class="tech-tags">
+              <li>
+                HTML
+              </li>
+              <li>
+                CSS
+              </li>
+              <li>
+                JavaScript
+              </li>
+              <li>
+                Laravel
+              </li>
+              <li>
+                PHP
+              </li>
+            </ul>
+          </div>
+        </div>
         <figure class="item-pics">
           <img src="{{ asset('images/desktop-ex-pic.jpg') }}" alt="" class="desktop-pic">
           <img src="{{ asset('images/mobile-ex-pic.png') }}" alt="" class="mobile-pic">
         </figure>
-        <div class="item-content">
-          <h3>
-            Example Company
-          </h3>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit Lorem ipsum dolor sit amet, consectetur adipiscing elit
-          </p>
-          <a href="#" class="button-link button-link--primary">Visit the website</a>
-          <ul class="tech-tags">
-            <li>
-              HTML
-            </li>
-            <li>
-              CSS
-            </li>
-            <li>
-              JavaScript
-            </li>
-            <li>
-              Laravel
-            </li>
-            <li>
-              PHP
-            </li>
-          </ul>
-        </div>
-      </li>
-      <li class="my-work__item">
-        <figure class="item-pics">
-          <img src="{{ asset('images/desktop-ex-pic.jpg') }}" alt="" class="desktop-pic">
-          <img src="{{ asset('images/mobile-ex-pic.png') }}" alt="" class="mobile-pic">
-        </figure>
-        <div class="item-content">
-          <h3>
-            Example Company
-          </h3>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit Lorem ipsum dolor sit amet, consectetur adipiscing elit
-          </p>
-          <a href="#" class="button-link button-link--primary">Visit the website</a>
-          <ul class="tech-tags">
-            <li>
-              HTML
-            </li>
-            <li>
-              CSS
-            </li>
-            <li>
-              JavaScript
-            </li>
-            <li>
-              Laravel
-            </li>
-            <li>
-              PHP
-            </li>
-          </ul>
-        </div>
-      </li>
-      <li class="my-work__item">
-        <figure class="item-pics">
-          <img src="{{ asset('images/desktop-ex-pic.jpg') }}" alt="" class="desktop-pic">
-          <img src="{{ asset('images/mobile-ex-pic.png') }}" alt="" class="mobile-pic">
-        </figure>
-        <div class="item-content">
-          <h3>
-            Example Company
-          </h3>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit Lorem ipsum dolor sit amet, consectetur adipiscing elit
-          </p>
-          <a href="#" class="button-link button-link--primary">Visit the website</a>
-          <ul class="tech-tags">
-            <li>
-              HTML
-            </li>
-            <li>
-              CSS
-            </li>
-            <li>
-              JavaScript
-            </li>
-            <li>
-              Laravel
-            </li>
-            <li>
-              PHP
-            </li>
-          </ul>
-        </div>
-      </li>
-      <li class="my-work__item">
-        <figure class="item-pics">
-          <img src="{{ asset('images/desktop-ex-pic.jpg') }}" alt="" class="desktop-pic">
-          <img src="{{ asset('images/mobile-ex-pic.png') }}" alt="" class="mobile-pic">
-        </figure>
-        <div class="item-content">
-          <h3>
-            Example Company
-          </h3>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit Lorem ipsum dolor sit amet, consectetur adipiscing elit
-          </p>
-          <a href="#" class="button-link button-link--primary">Visit the website</a>
-          <ul class="tech-tags">
-            <li>
-              HTML
-            </li>
-            <li>
-              CSS
-            </li>
-            <li>
-              JavaScript
-            </li>
-            <li>
-              Laravel
-            </li>
-            <li>
-              PHP
-            </li>
-          </ul>
-        </div>
-      </li>
-      <li class="my-work__item">
-        <figure class="item-pics">
-          <img src="{{ asset('images/desktop-ex-pic.jpg') }}" alt="" class="desktop-pic">
-          <img src="{{ asset('images/mobile-ex-pic.png') }}" alt="" class="mobile-pic">
-        </figure>
-        <div class="item-content">
-          <h3>
-            Example Company
-          </h3>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit Lorem ipsum dolor sit amet, consectetur adipiscing elit
-          </p>
-          <a href="#" class="button-link button-link--primary">Visit the website</a>
-          <ul class="tech-tags">
-            <li>
-              HTML
-            </li>
-            <li>
-              CSS
-            </li>
-            <li>
-              JavaScript
-            </li>
-            <li>
-              Laravel
-            </li>
-            <li>
-              PHP
-            </li>
-          </ul>
-        </div>
       </li>
     </ul>
   </div>
