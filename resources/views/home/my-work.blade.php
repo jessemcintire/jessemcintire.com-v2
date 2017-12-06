6a220ba629219520461006102f42b3ef02bf2812
Jesse McIntire <jessemcintire2@gmail.com>
2017-12-06 11:48:04 -0500

    fill out portfolio with screenshots; add nextdroid to portfolio

--- resources/views/home/my-work.blade.php
+++ resources/views/home/my-work.blade.php
@@ -19,20 +19,20 @@
               Jay Cardiello is one of the most sought after and recognized fitness professionals in the world.
             </p>
             <p>
-              I worked to turn his design into a fully responsive website. Deliverables included the markup, styling, animations and sever-side structuring.
+              I turned his website design into a fully responsive website. Deliverables included the markup, styling, animations and sever-side structuring.
             </p>
           </div>
           <div class="item-info">
-            <!-- <a href="#" class="button-link button-link--primary">
-              Visit the website
+            <a href="http://www.jaycardiello.com/" class="button-link button-link--primary">
+              Visit Website
               <img src="{{ asset('images/svg/thin-arrow-right-teal.svg') }}" alt="">
-            </a> -->
+            </a>
             <ul class="tech-tags">
               <li>
                 HTML
               </li>
               <li>
-                CSS
+                CSS (Sass)
               </li>
               <li>
                 JavaScript
@@ -47,8 +47,69 @@
           </div>
         </div>
         <figure class="item-pics">
-          <img src="{{ asset('images/desktop-ex-pic.jpg') }}" alt="" class="desktop-pic">
-          <img src="{{ asset('images/mobile-ex-pic.png') }}" alt="" class="mobile-pic">
+          <img src="{{ asset('images/portfolio/jay-cardiello/homepage-hero-desktop.jpg') }}" alt="" class="desktop-pic">
+          <img src="{{ asset('images/portfolio/jay-cardiello/homepage-hero-mobile.png') }}" alt="" class="mobile-pic">
+        </figure>
+        <figure class="item-pics">
+          <img src="{{ asset('images/portfolio/jay-cardiello/homepage-body-desktop.jpg') }}" alt="" class="desktop-pic">
+          <img src="{{ asset('images/portfolio/jay-cardiello/homepage-body-mobile.png') }}" alt="" class="mobile-pic">
+        </figure>
+        <figure class="item-pics">
+          <img src="{{ asset('images/portfolio/jay-cardiello/footer-desktop.jpg') }}" alt="" class="desktop-pic">
+          <img src="{{ asset('images/portfolio/jay-cardiello/footer-mobile.png') }}" alt="" class="mobile-pic">
+        </figure>
+        <figure class="item-pics">
+          <img src="{{ asset('images/portfolio/jay-cardiello/my-journey-desktop.jpg') }}" alt="" class="desktop-pic">
+          <img src="{{ asset('images/portfolio/jay-cardiello/my-journey-mobile.png') }}" alt="" class="mobile-pic">
+        </figure>
+      </li>
+
+      <li class="my-work__item">
+        <div class="item-content">
+          <div class="item-description">
+            <h3>
+              NEXTDROID
+            </h3>
+            <p>
+              Nextdroid is a leading developer of advanced robotics systems including fully automated cars and submarines            </p>
+            <p>
+              I provided them with development expertise to turn their design into fully responsive and scalable code. Pixel perfect markup and optimized media resources were primary concerns that I addressed.            </p>
+          </div>
+          <div class="item-info">
+            <a href="http://162.243.171.149/" class="button-link button-link--primary">
+              Visit Website
+              <img src="{{ asset('images/svg/thin-arrow-right-teal.svg') }}" alt="">
+            </a>
+            <ul class="tech-tags">
+              <li>
+                HTML
+              </li>
+              <li>
+                CSS (Sass)
+              </li>
+              <li>
+                JavaScript
+              </li>
+              <li>
+                Laravel
+              </li>
+              <li>
+                Pixel Perfect Development
+              </li>
+            </ul>
+          </div>
+        </div>
+        <figure class="item-pics">
+          <img src="{{ asset('images/portfolio/nextdroid/homepage-hero-desktop-2.jpg') }}" alt="" class="desktop-pic">
+          <img src="{{ asset('images/portfolio/nextdroid/homepage-hero-mobile.png') }}" alt="" class="mobile-pic">
+        </figure>
+        <figure class="item-pics">
+          <img src="{{ asset('images/portfolio/nextdroid/body-desktop.jpg') }}" alt="" class="desktop-pic">
+          <img src="{{ asset('images/portfolio/nextdroid/body-mobile.png') }}" alt="" class="mobile-pic">
+        </figure>
+        <figure class="item-pics">
+          <img src="{{ asset('images/portfolio/nextdroid/footer-desktop.jpg') }}" alt="" class="desktop-pic">
+          <img src="{{ asset('images/portfolio/nextdroid/footer-mobile.png') }}" alt="" class="mobile-pic">
         </figure>
       </li>
     </ul>
